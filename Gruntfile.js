8eb8cbbea4e856b4f847bd236c2abdabae23cb3e
toran billups <toranb@gmail.com>
2014-04-09 23:01:53 -0500

    added step 3 precompile hbs templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-ember-template-compiler');
 
   grunt.initConfig({
     concat: {
@@ -8,11 +9,24 @@ module.exports = function(grunt) {
             'js/vendor/jquery/jquery.min.js',
             'js/vendor/handlebars/handlebars.js',
             'js/vendor/ember/ember.min.js',
-            'js/app.js'],
+            'js/app.js',
+            'js/dist/tmpl.min.js'],
           dest: 'js/dist/deps.min.js'
       }
+    },
+    emberhandlebars: {
+        compile: {
+            options: {
+                templateName: function(sourceFile) {
+                    var newSource = sourceFile.replace('js/templates/', '');
+                    return newSource.replace('.handlebars', '');
+                }
+            },
+            files: ['js/templates/**/*.handlebars'],
+            dest: 'js/dist/tmpl.min.js'
+        }
     }
   });
 
-  grunt.task.registerTask('local', ['concat:dist']);
+  grunt.task.registerTask('local', ['emberhandlebars', 'concat:dist']);
 }
